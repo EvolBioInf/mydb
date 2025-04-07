c73616bb758949d9bf735edbe40201232b053b10
Bernhard Haubold <haubold@evolbio.mpg.de>
2025-04-07 16:46:48 +0200

    Edited package

--- mysql/mycli.sh
+++ mysql/mycli.sh
@@ -3,7 +3,9 @@ curl https://dev.mysql.com/downloads/repo/apt/ |
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.33-1_all.deb
 sudo dpkg -i mysql-apt-config_0.8.33-1_all.deb
 sudo apt update
-sudo atp install mysql-community-server
-mysql -e "show global variables like 'local_infile'"
-mysql -e "set global local_infile=true"
-mysql -e "show global variables like 'local_infile'"
+sudo apt install mysql-community-server
+mysql -u anonymous \
+        -e "show global variables like 'local_infile'"
+mysql -u anonymous -e "set global local_infile=true"
+mysql -u anonymous \
+        -e "show global variables like 'local_infile'"
